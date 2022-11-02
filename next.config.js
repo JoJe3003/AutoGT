f4a6c3526ae77a8992c9ffe29975322de26c371d
JoJe3003 <jfjespersen@gmail.com>
2022-11-02 01:46:03 +0100

    Added photo parser and uploader

--- next.config.js
+++ next.config.js
@@ -2,6 +2,34 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'billink3.blob.core.windows.net',
+				port: '',
+				pathname: '/blob5/image/2212/thumb/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'billink.no',
+				port: '',
+				pathname: '/image/2212/thumb/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'billink3.blob.core.windows.net',
+				port: '',
+				pathname: '/blob5/image/2212/annonser/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'billink.no',
+				port: '',
+				pathname: '/image/2212/annonser/**',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
--- pages/inventar.js
+++ pages/inventar.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import React from 'react';
-import { xml2json } from 'xml-js';
+// import { xml2json } from 'xml-js';
 
-const axios = require('axios');
+// const axios = require('axios');
+// const xml2js = require('xml2js');
 const cheerio = require('cheerio');
-const xml2js = require('xml2js');
 
 const xmlFile =
 	'<food><name>Belgian Waffles</name><price>$5.95</price><description>Two of our famous Belgian Waffles with plenty of real maple syrup</description><calories>650</calories></food>';
@@ -15,7 +16,12 @@ function inventar({ data }) {
 			<Head>
 				<title>Auto GT | Inventar</title>
 			</Head>
-			<div>{data}</div>
+			<h1 className="h-screen my-auto text-center text-7xl">IN DEVELOPMENT</h1>
+			<div className="inline max-w-xs">
+				{data.map((e) => (
+					<Image key={e.index} src={e} alt="Pic of car" width="1773px" height="1182px" />
+				))}
+			</div>
 		</>
 	);
 }
@@ -24,19 +30,16 @@ export async function getServerSideProps() {
 	const res = await fetch(
 		'https://billink.no/page2_xml.php?kode=3baa6aaa-a8c2-4565-b6a4-4b4f4c33b5b0&butikk=agt&detaljert=1'
 	);
-	const xml = await res.text();
-	// console.log(xml);
-	const jsonObject = xml2json(xml);
 
-	// console.log(jsonObject);
+	const xmlText = await res.text();
+	const $ = cheerio.load(xmlText, { xmlMode: true });
 
-	const data = jsonObject;
-	// console.log('LOGGED -> ' + JSON.stringify(res.ok));
-	// console.dir('DIR -> ' + JSON.stringify(res.url));
-	// const xmlText = await res.text();
-	// const $ = cheerio.load(xmlText, { xmlMode: true });
-
-	// var data = $('ANNONSE').first().children('BILDE').text();
+	var data = $('BILDE')
+		.children('URL')
+		.map(function (i, el) {
+			return $(this).text();
+		})
+		.toArray();
 
 	// console.log(data);
 
