51bc731ff0a75c33943e286b094f716306be7fdb
JoJe3003 <jfjespersen@gmail.com>
2022-12-10 01:55:21 +0100

    Better randomiser

--- pages/index.js
+++ pages/index.js
@@ -119,8 +119,26 @@ export async function getServerSideProps() {
 		})
 		.toArray();
 
-	let arrBiler = biler.reverse().sort(() => Math.random() - 0.5);
+	function shuffle(array) {
+		let currentIndex = array.length,
+			randomIndex;
 
+		// While there remain elements to shuffle.
+		while (currentIndex != 0) {
+			// Pick a remaining element.
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+
+			// And swap it with the current element.
+			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+		}
+
+		return array;
+	}
+
+	let arrBiler = biler.reverse();
+
+	shuffle(arrBiler);
 	// Post data
 
 	return { props: { arrBiler } };
